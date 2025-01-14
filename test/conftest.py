9bec63da347e8ece0b44141be344c14278617594
maximQA777 <strogov.maximik@yandex.ru>
2025-01-14 23:06:31 +0300

    Добавил чтоб тест пошел , когда загрузился html

--- test/conftest.py
+++ test/conftest.py
@@ -6,11 +6,11 @@ from selenium import webdriver
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
     driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'   #чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options  #чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    #driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер
+    #driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
     #driver_options.add_argument('--headless')
-    #browser.config.driver_options = driver_options
     yield
     browser.quit()
