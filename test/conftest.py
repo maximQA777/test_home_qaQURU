7af49048bfc434e228d47d204df9ce16a287d379
Maksim Strgogv <alabai.vfrcbv2001@gmail.com>
2025-02-22 15:53:39 +0300

    Форматирование строк

--- test/conftest.py
+++ test/conftest.py
@@ -7,10 +7,10 @@ from selenium import webdriver
 def setup_browser():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options  #чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
+    browser.config.driver_options = driver_options  # чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    #driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
-    #driver_options.add_argument('--headless')
+    # driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
+    # driver_options.add_argument('--headless')
     yield
     browser.quit()
