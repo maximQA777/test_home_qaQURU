614f707acb6ba9b33b7463df41f74bc6da8c36eb
Maksim Strgogv <alabai.vfrcbv2001@gmail.com>
2025-03-02 18:15:07 +0300

    Исправление к уроку PageObjects

---
+++ mid_level_step_objects/page.py
@@ -0,0 +1,89 @@
+import os
+
+from selene import browser, by, have
+
+class RegistrationPage:
+    def open(self):
+        browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+        browser.open('/')
+
+    def set_firstname(self, value):
+        browser.element('#firstName').type(value)
+
+    def set_lastname(self, value):
+        browser.element('#lastName').type(value)
+
+    def set_email(self, email):
+        browser.element('#userEmail').type(email)
+
+    def set_gender(self, gender_value):
+        browser.element(f'[for="gender-radio-{gender_value}"]').click()
+
+    def set_phone(self, phone):
+        browser.element('#userNumber').type(phone)
+
+    def set_birthday(self, month, year, day):
+        browser.element('#dateOfBirthInput').click()
+        browser.element(f'.react-datepicker__month-select option[value="{month}"]').click()
+        browser.element(f'.react-datepicker__year-select option[value="{year}"]').click()
+        browser.element(f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)').click()
+
+    def scroll_page(self):
+        browser.execute_script('window.scrollBy(0, 400);')  # Скролл, чтобы не убирать рекламу
+
+    def set_subjects(self, *subjects):
+        for subject in subjects:
+            browser.element('#subjectsInput').type(subject).press_enter()
+
+    def set_hobby(self, hobby_value):
+        browser.element(f'[for="hobbies-checkbox-{hobby_value}"]').click()
+
+    def upload_picture(self, file_path):
+        browser.element('#uploadPicture').send_keys(os.path.abspath(file_path))
+
+    def set_address(self, address, state, city):
+        browser.element('#currentAddress').type(address)
+        browser.element('#state').click().element(by.text(state)).click()
+        browser.element('#city').click().element(by.text(city)).click()
+
+    def click_submit_button(self):
+        browser.element('#submit').click()
+
+    def assert_filled_names(self, Names):
+        browser.element('.table-responsive').should(have.text(Names))
+
+    def assert_text_present(self, text):
+        browser.element('.table-responsive').should(have.text(text))
+
+    def assert_filled_name(self, name):
+        self.assert_text_present(name)
+
+    def assert_filled_label(self, label):
+        self.assert_text_present(label)
+
+    def assert_filled_email(self, email):
+        self.assert_text_present(email)
+
+    def assert_filled_gender(self, gender):
+        self.assert_text_present(gender)
+
+    def assert_filled_mobile(self, mobile):
+        self.assert_text_present(mobile)
+
+    def assert_filled_subject(self, subject):
+        self.assert_text_present(subject)
+
+    def assert_filled_hobbies(self, hobbies):
+        self.assert_text_present(hobbies)
+
+    def assert_filled_picture(self, picture):
+        self.assert_text_present(picture)
+
+    def assert_filled_address(self, address):
+        self.assert_text_present(address)
+
+    def assert_filled_state(self, state):
+        self.assert_text_present(state)
+
+    def assert_filled_birth(self, day):
+        self.assert_text_present(day)
---
+++ test/test_demoqa.py
@@ -0,0 +1,43 @@
+
+
+from mid_level_step_objects.page import RegistrationPage
+
+
+
+
+
+def test_register_form():
+    registration_form = RegistrationPage()
+
+    registration_page = RegistrationPage()
+    registration_page.open()
+    registration_page.set_firstname("Maxim")
+    registration_page.set_lastname("Lerich")
+    registration_page.set_email("alab@mail.me")
+    registration_page.set_gender(2)
+    registration_page.set_phone("3123213131")
+    registration_page.set_birthday("5", "1990", "15")
+    registration_page.scroll_page()
+    registration_page.set_subjects("Maths", "English")
+    registration_page.set_hobby(1)
+    registration_page.upload_picture("../resources/picture.png")
+    registration_page.set_address("Kabanina Polina", "NCR", "Delhi")
+
+    registration_form.click_submit_button()
+
+    registration_form.assert_filled_label('Label Values')
+    registration_form.assert_filled_names('Student Name Maxim Lerich')
+    registration_form.assert_filled_email('Student Email alab@mail.me')
+    registration_form.assert_filled_gender('Gender Female')
+    registration_form.assert_filled_mobile('Mobile 3123213131')
+    registration_form.assert_filled_birth('Date of Birth 15 June,1990')
+    registration_form.assert_filled_subject('Subjects Maths, English')
+    registration_form.assert_filled_hobbies('Hobbies Sports')
+    registration_form.assert_filled_picture('Picture picture.png')
+    registration_form.assert_filled_address('Address Kabanina Polina')
+    registration_form.assert_filled_state('State and City NCR Delhi')
+
+
+
+
+
