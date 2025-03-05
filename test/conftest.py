3a1028977c6150cf6ae3c70086d249c6ec3e87c0
Maksim Strgogv <alabai.vfrcbv2001@gmail.com>
2025-03-05 17:07:01 +0300

    Jenkins

--- test/conftest.py
+++ test/conftest.py
@@ -44,7 +44,16 @@ selenoid_login = os.getenv("SELENOID_LOGIN")
 selenoid_pass = os.getenv("SELENOID_PASS")
 selenoid_url = os.getenv("SELENOID_URL")
 
-
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options  # чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    # driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
+    # driver_options.add_argument('--headless')
 
 
 
