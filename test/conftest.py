edf77fcc9db5f03fb64b90fdf400fdd6b53bf360
Maksim Strgogv <alabai.vfrcbv2001@gmail.com>
2025-03-05 17:52:06 +0300

    Jenkins

--- test/conftest.py
+++ test/conftest.py
@@ -10,42 +10,6 @@ from selenium.webdriver.chrome.options import Options
 from test import attach
 
 
-@pytest.fixture(scope='function')
-def jenkins_browser(request):
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": False
-        }
-    }
-
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-        options=options)
-
-    browser.config.driver = driver
-
-    yield browser
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    browser.quit()
-
-
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
-
-
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
-
-
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
@@ -56,3 +20,22 @@ def setup_browser():
     browser.config.window_height = 1080
     # driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
     # driver_options.add_argument('--headless')
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "125.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    browser.config.driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+
+    yield
+    browser.quit()
