31c646abafd5172f6e39cf8f9848396b16207919
Maksim Strgogv <alabai.vfrcbv2001@gmail.com>
2025-03-05 16:15:46 +0300

    Jenkins

--- test/conftest.py
+++ test/conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from test import attach
-
+from dotenv import load_dotenv
 
 @pytest.fixture(scope='function')
 def jenkins_browser(request):
@@ -15,16 +15,16 @@ def jenkins_browser(request):
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": True
+            "enableVideo": False
         }
     }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
 
-    browser = Browser(Config(driver))
+    options.capabilities, update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
 
     yield browser
     attach.add_screenshot(browser)
@@ -32,6 +32,16 @@ def jenkins_browser(request):
     attach.add_html(browser)
     browser.quit()
 
+
+import os
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
 
 
